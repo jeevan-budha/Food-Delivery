e14e3fbfaf58c06959d4b05795f1fd8bc78a7623
jeevan-budha <jeevan.7budha@gmail.com>
2025-07-14 15:34:00 +0545

    project completed

--- app.js
+++ app.js
@@ -12,8 +12,18 @@ const cartTab = document.querySelector(".cart-tab");
 const closeBtn = document.querySelector(".close-btn");
 const cartList = document.querySelector(".card-list");
 const cartPieceList = document.querySelector(".cart-list");
+const cartTotal = document.querySelector(".cart-total");
+const cartValue = document.querySelector(".cart-value");
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-menu");
+const bars = document.querySelector(".fa-bars");
 
-
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("mobile-menu-active");
+})
+hamburger.addEventListener("click", () => {
+  bars.classList.toggle("fa-xmark");
+})
 
 cartIcon.addEventListener("click", () => {
   cartTab.classList.add("cart-tab-active");
@@ -24,7 +34,23 @@ closeBtn.addEventListener("click", () => {
 });
 
 let prodcutList = [];
-let cartProduct =[];
+let cartProduct = [];
+
+const updateTotals = () => {
+  let totalPrice = 0;
+  let totalQuantity = 0;
+  document.querySelectorAll(".item").forEach(item => {
+
+    const quantity = parseInt(item.querySelector(".quantity-value").textContent);
+
+    const price = parseFloat(item.querySelector(".item-total").textContent.replace('$', ''));
+    totalPrice += price;
+    totalQuantity += quantity;
+
+  });
+  cartTotal.textContent = `$${totalPrice.toFixed(2)}`;
+  cartValue.textContent = totalQuantity;
+}
 
 const showCart = () => {
   prodcutList.forEach(product => {
@@ -39,28 +65,30 @@ const showCart = () => {
       <h4 h4 class="price">${product.price}</h4>
       <a href="#" class="btn card-btn">Add to Cart</a>`;
 
-      cartList.appendChild(orderCard);
+    cartList.appendChild(orderCard);
 
-      const cardBtn = orderCard.querySelector(".card-btn");
-      cardBtn.addEventListener("click",(e)=>{
-        e.preventDefault();
-        addToCart(product);
-      })
+    const cardBtn = orderCard.querySelector(".card-btn");
+    cardBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      addToCart(product);
+    })
   });
 }
 
-const addToCart =(product)=>{
-  const existingProduct =cartProduct.find(item=>item.id===product.id);
-  if(existingProduct){
+const addToCart = (product) => {
+  const existingProduct = cartProduct.find(item => item.id === product.id);
+  if (existingProduct) {
     alert("Item Already in your cart !");
     return;
   }
   cartProduct.push(product);
+  let quantity = 1;
+  let price = parseFloat(product.price.replace("$", ""));
 
   const cartItem = document.createElement("div");
   cartItem.classList.add("item");
 
-  cartItem.innerHTML=`
+  cartItem.innerHTML = `
   <div class="item-image">
      <img src="${product.image}" alt="">
    </div>
@@ -69,17 +97,50 @@ const addToCart =(product)=>{
       <h4 class="item-total">${product.price}</h4>
     </div>
     <div class="flex">
-      <a href="#" class="quantity-btn">
+      <a href="#" class="quantity-btn minus">
         <i class="fa-solid fa-minus"></i>
       </a>
-      <h4 class="quantity-value">4</h4>
-      <a href="#" class="quantity-btn">
+      <h4 class="quantity-value">${quantity}</h4>
+      <a href="#" class="quantity-btn plus">
         <i class="fa-solid fa-plus"></i>
       </a>
     </div>`;
 
-    cartPieceList.appendChild(cartItem);
+  cartPieceList.appendChild(cartItem);
+  updateTotals();
 
+  const plusBtn = cartItem.querySelector(".plus");
+  const quantityValue = cartItem.querySelector(".quantity-value");
+  const itemTotal = cartItem.querySelector(".item-total");
+  const minusBtn = cartItem.querySelector(".minus");
+
+  plusBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    quantity++;
+    quantityValue.textContent = quantity;
+    itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+    updateTotals();
+  })
+
+  minusBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (quantity > 1) {
+      quantity--;
+      quantityValue.textContent = quantity;
+      itemTotal.textContent = `$${(price * quantity).toFixed(2)}`;
+      updateTotals();
+    }
+    else {
+      cartItem.classList.add("slide-out")
+      setTimeout(() => {
+        cartItem.remove();
+        cartProduct = cartProduct.filter(item => item.id !== product.id);
+        updateTotals();
+      }, 300)
+
+    }
+
+  })
 }
 
 const initApp = () => {
